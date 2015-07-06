af82c083e4e764667e8d887f9c8d286a745960d2
sohan1 <shn619@gmail.com>
2015-07-06 15:31:00 +0530

    changes

--- application/views/frontend/contact.php
+++ application/views/frontend/contact.php
@@ -11,13 +11,20 @@
     <div class="row">
         <div class="col-md-4">
             <div class="contact-add">
-                <h5>ADDRESS</h5>
-                <h5>DESIGN HOTEL CHENNAI</h5>
+                <h4>ADDRESS</h4>
+                <h4>DESIGN HOTEL CHENNAI</h4>
                 <p>Phoenix Marketcity</p>
                 <p>142, Velachery Main Road, Near Gurunanak College, </p>
                 <p>Velachery, Chennai, Tamil Nadu 600 042.</p>
+                <p class="pad-tp">Tel: +91 44 33161718</p>
+                <p class="pad-tp">Fax: +91 44 33161717</p>
+                
             </div>
         </div>
-        <div class="col-md-8"></div>
+        <div class="col-md-8">
+           <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.6970871836966!2d80.2164684!3d12.991216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a526763b48e60eb%3A0xdb3a29009036c251!2sPhoenix+Marketcity!5e0!3m2!1sen!2sin!4v1436176099978" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
     </div>
 </div>
