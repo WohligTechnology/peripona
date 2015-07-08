ea9dec82e59471221b09f7deddce65eb0b8a04e2
mahesh-wohlig <mahesh@wohlig.com>
2015-07-08 11:53:20 +0530

    done

--- application/views/frontend/amenities.php
+++ application/views/frontend/amenities.php
@@ -9,24 +9,173 @@
 </div>
 <div class="container">
     <div class="row">
-             <div class="overview-cont text-center">
+        <div class="overview-cont text-center">
             <p>
                 Awaiting your presence at Design Hotel Chennai is a broad array of amenities & services designed with your comfort in mind. Enjoy South Indian delicacies& multicultural cuisine at the exclusive Annam Café, offering full dining & refreshments in a stunning venue. Our staff eagerly anticipates your stay!
             </p>
         </div>
     </div>
-    <div class="row">
+    <div class="row margin-tp">
         <div class="col-md-3">
             <div class="bath-set text-center">
-                <img src="<?php echo base_url('frontassets/image/amenites/bath.png'); ?>"><br>
+                <img src="<?php echo base_url('frontassets/image/amenites/bath.png'); ?>">
+                <br>
                 <span>BATHROOM</span>
                 <div class="mini-circle">
 
-        </div>
+                </div>
+            </div>
+            <div class="ul-list">
+                <ul style="list-style-type:disc">
+                    <li>Forest Essentials™ Luxurious Ayurveda Bath Amenities</li>
+                    <li>Make-up/Shaving Mirror</li>
+                    <li>Bedroom Slippers</li>
+                    <li>Hairdryer</li>
+                </ul>
             </div>
         </div>
-        <div class="col-md-3"></div>
-        <div class="col-md-3"></div>
-        <div class="col-md-3"></div>
+        <div class="col-md-3">
+            <div class="bath-set text-center">
+                <img src="<?php echo base_url('frontassets/image/amenites/bed.png'); ?>">
+                <br>
+                <span>BED FEATURES</span>
+                <div class="mini-circle">
+
+                </div>
+            </div>
+            <div class="ul-list">
+                <ul style="list-style-type:disc">
+                    <li>Welspun™ Duvet & Bedding</li>
+                    <li>Hush™Mattress & Pillows</li>
+
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="bath-set text-center">
+                <img src="<?php echo base_url('frontassets/image/amenites/diner.png'); ?>">
+                <br>
+                <span>ROOM AMENITIES & SERVICES</span>
+                <div class="mini-circle">
+
+                </div>
+            </div>
+            <div class="ul-list">
+                <ul style="list-style-type:disc">
+                    <li>Personalized Wake-Up Service (On Request)</li>
+                    <li>In-Room Dining</li>
+                    <li>Iron/Ironing Board (On Request)</li>
+                    <li>Minibar & Fridge</li>
+                    <li>Tea kettle & Coffee maker</li>
+                    <li>Ergonomic Work Desk and Chair</li>
+                    <li>In-Room Electronic Safe</li>
+                </ul>
+            </div>
+        </div>
+       <div class="col-md-3">
+            <div class="bath-set text-center">
+                <img src="<?php echo base_url('frontassets/image/amenites/tv.png'); ?>">
+                <br>
+                <span>ENTERTAINMENT</span>
+                <div class="mini-circle">
+
+                </div>
+            </div>
+            <div class="ul-list">
+                <ul style="list-style-type:disc">
+                    <li>42-Inch Flat Screen LCD 
+TV (3D TV in Maya)</li>
+                    <li>Complimentary Daily 
+Newspaper (National)</li>
+                    <li>Movie Screenings</li>
+                
+                </ul>
+            </div>
+        </div>
+    </div>
+        <div class="row margin-tp">
+        <div class="col-md-3">
+            <div class="bath-set text-center">
+                <img src="<?php echo base_url('frontassets/image/amenites/hotel.png'); ?>">
+                <br>
+                <span>DINING</span>
+                <div class="mini-circle">
+
+                </div>
+            </div>
+            <div class="ul-list">
+                <ul style="list-style-type:disc">
+                    <li>Breakfast, Lunch & Dinner 
+at Annam Café</li>
+                    <li>In-Room Dining</li>
+                    <li>Mini Bar</li>
+                    
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="bath-set text-center">
+                <img src="<?php echo base_url('frontassets/image/amenites/work.png'); ?>">
+                <br>
+                <span>OFFICE/TELECOM</span>
+                <div class="mini-circle">
+
+                </div>
+            </div>
+            <div class="ul-list">
+                <ul style="list-style-type:disc">
+                    <li>Conference Room, Seating 
+Eight (8)</li>
+                    <li>Video Conferencing</li>
+                    <li>HDMI Connection </li>
+                    <li>Media Hub </li>
+
+
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="bath-set text-center">
+                <img src="<?php echo base_url('frontassets/image/amenites/wifi.png'); ?>">
+                <br>
+                <span>INTERNET ACCESS</span>
+                <div class="mini-circle">
+
+                </div>
+            </div>
+            <div class="ul-list">
+                <ul style="list-style-type:disc">
+                    <li>Free Wireless High Speed 
+Internet Access throughout 
+Hotel</li>
+                    
+                </ul>
+            </div>
+        </div>
+       <div class="col-md-3">
+            <div class="bath-set text-center">
+                <img src="<?php echo base_url('frontassets/image/amenites/lunch.png'); ?>">
+                <br>
+                <span>HOTEL SERVICES</span>
+                <div class="mini-circle">
+
+                </div>
+            </div>
+            <div class="ul-list">
+                <ul style="list-style-type:disc">
+                    <li>24-Hour Security</li>
+                    <li>Valet Parking Service</li>
+                    <li>Paid Transportation On-Call</li>
+                    <li>Staffed Business Center</li>
+                    <li>Stamps and Mailing Facilities</li>
+                    <li>Left Luggage Facilities</li>
+                    <li>Shoe Cleaning Services</li>
+                    <li>Newspaper</li>
+                
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
