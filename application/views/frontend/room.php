5fd132b1e78279698cba80d19b379ec3ee6ce563
sapanab <basutkar.sapana3@gmail.com>
2015-07-09 15:47:28 +0530

    room page changes

--- application/views/frontend/room.php
+++ application/views/frontend/room.php
@@ -1,64 +1,69 @@
 <div class="container">
-    
-<div class="row">
 
-   <div class="overview-cont text-center">
-    <h2>More than just a hotel
+    <div class="row">
+
+        <div class="overview-cont text-center">
+            <h2>More than just a hotel
 
 </h2>
-    <p>
-        Our architectural detail is but one element that defines Design Hotel Chennai as one of the best luxury boutique hotels in India – and certainly its most uncommon. In line with our commitment to offer our guests a diversity of quality choice, our 26 well-appointed guest rooms are elegantly designed according to the bold artistic theme of our four culturally distinct corridors.
-    </p>
-</div>
-</div>
-<div class="row">
-   <div class="col-md-3">
-       <div class="room-side">
-           <img src="<?php echo base_url('frontassets/image/room1.png'); ?>" class="img-responsive">
-       </div>
-   </div>
-   <div class="col-md-9">
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-    <li data-target="#myCarousel" data-slide-to="3"></li>
-  </ol>
+            <p>
+                Our architectural detail is but one element that defines Design Hotel Chennai as one of the best luxury boutique hotels in India – and certainly its most uncommon. In line with our commitment to offer our guests a diversity of quality choice, our 26 well-appointed guest rooms are elegantly designed according to the bold artistic theme of our four culturally distinct corridors.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 padding0">
+            <div class="room-side">
+                <h1>ati</h1>
+                <p>The tasteful, monochrome Ati Corridor has manifestations of the elegant and rich Kolam Rice art, or 'Rangolee'adorning it. Black inlaid on gray, alternating with white stripes, the composition is dense and intricate, quint.</p>
+                <div class="wrap-btn">
+                    <button class="button enter-btn">ENTER
+                        <div class="all-btns"></div>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9 padding0">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-<!--
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <!--
     <div class="item ">
       <img src="<?php echo base_url('frontassets/image/slider/slide1.jpg'); ?>" alt="Chania">
     </div>
 -->
 
-    <div class="item active">
-      <img src="<?php echo base_url('frontassets/image/slider/slide2.jpg'); ?>" alt="Chania">
-    </div>
+                    <div class="item active">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide2.jpg'); ?>" alt="Chania">
+                    </div>
 
-    <div class="item">
-      <img src="<?php echo base_url('frontassets/image/slider/slide3.jpg'); ?>" alt="Flower">
-    </div>
+                    <div class="item">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide3.jpg'); ?>" alt="Flower">
+                    </div>
 
-    <div class="item">
-      <img src="<?php echo base_url('frontassets/image/slider/slide4.jpg'); ?>" alt="Flower">
-    </div>
-  </div>
+                    <div class="item">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide4.jpg'); ?>" alt="Flower">
+                    </div>
+                </div>
 
-  <!-- Left and right controls -->
-  <a class=" carousel-controls" href="#myCarousel" role="button" data-slide="prev">
+                <!-- Left and right controls -->
+                <a class=" carousel-controls" href="#myCarousel" role="button" data-slide="prev">
   <img class="left" src="<?php echo base_url('frontassets/image/left.jpg'); ?>" alt="Flower">
 
   </a>
-  <a class=" carousel-controls" href="#myCarousel" role="button" data-slide="next">
+                <a class=" carousel-controls" href="#myCarousel" role="button" data-slide="next">
    <img class="right" src="<?php echo base_url('frontassets/image/right.jpg'); ?>" alt="Flower">
 <!--    <span class="sr-only">Next</span>-->
   </a>
+            </div>
+        </div>
+    </div>
 </div>
-</div>
-</div>
-</div>
-   
