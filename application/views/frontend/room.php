e5feac73decaa66c3a9afdc8ad5c14aecb1d739d
sapanab <basutkar.sapana3@gmail.com>
2015-07-09 17:35:37 +0530

    home page done

--- application/views/frontend/room.php
+++ application/views/frontend/room.php
@@ -13,7 +13,7 @@
     </div>
     <div class="row">
         <div class="col-md-3 padding0">
-            <div class="room-side">
+            <div class="room-side ati">
                 <h1>ati</h1>
                 <p>The tasteful, monochrome Ati Corridor has manifestations of the elegant and rich Kolam Rice art, or 'Rangolee'adorning it. Black inlaid on gray, alternating with white stripes, the composition is dense and intricate, quint.</p>
                 <div class="wrap-btn">
@@ -65,5 +65,198 @@
   </a>
             </div>
         </div>
+    </div>
+     <div class="row mt40">
+        <div class="col-md-3 padding0">
+            <div class="room-side maya">
+                <h1>maya</h1>
+                <p>Traveling along the Maya Corridor transports you to a vibrant world true to its mystical name. The centerpiece of the seven (7) rooms located within is a stunning lenticular Tanjore panel painting, spanning the length.</p>
+                <div class="wrap-btn">
+                    <button class="button enter-btn">ENTER
+                        <div class="all-btns"></div>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9 padding0">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <!--
+    <div class="item ">
+      <img src="<?php echo base_url('frontassets/image/slider/slide1.jpg'); ?>" alt="Chania">
+    </div>
+-->
+
+                    <div class="item active">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide2.jpg'); ?>" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide3.jpg'); ?>" alt="Flower">
+                    </div>
+
+                    <div class="item">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide4.jpg'); ?>" alt="Flower">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class=" carousel-controls" href="#myCarousel" role="button" data-slide="prev">
+  <img class="left" src="<?php echo base_url('frontassets/image/left.jpg'); ?>" alt="Flower">
+
+  </a>
+                <a class=" carousel-controls" href="#myCarousel" role="button" data-slide="next">
+   <img class="right" src="<?php echo base_url('frontassets/image/right.jpg'); ?>" alt="Flower">
+<!--    <span class="sr-only">Next</span>-->
+  </a>
+            </div>
+        </div>
+    </div>
+     <div class="row mt40">
+        <div class="col-md-3 padding0">
+            <div class="room-side nunya">
+                <h1>nunya</h1>
+                <p>The Nunya Corridor gives testament to the power and beauty of simplicity. Finished in off-white laminate, the passageway features a mirror at the end, giving an illusion of an infinity corridor that conditions the senses.</p>
+                <div class="wrap-btn">
+                    <button class="button enter-btn">ENTER
+                        <div class="all-btns"></div>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9 padding0">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <!--
+    <div class="item ">
+      <img src="<?php echo base_url('frontassets/image/slider/slide1.jpg'); ?>" alt="Chania">
+    </div>
+-->
+
+                    <div class="item active">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide2.jpg'); ?>" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide3.jpg'); ?>" alt="Flower">
+                    </div>
+
+                    <div class="item">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide4.jpg'); ?>" alt="Flower">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class=" carousel-controls" href="#myCarousel" role="button" data-slide="prev">
+  <img class="left" src="<?php echo base_url('frontassets/image/left.jpg'); ?>" alt="Flower">
+
+  </a>
+                <a class=" carousel-controls" href="#myCarousel" role="button" data-slide="next">
+   <img class="right" src="<?php echo base_url('frontassets/image/right.jpg'); ?>" alt="Flower">
+<!--    <span class="sr-only">Next</span>-->
+  </a>
+            </div>
+        </div>
+    </div>
+     <div class="row mt40">
+        <div class="col-md-3 padding0">
+            <div class="room-side roop">
+                <h1>roop</h1>
+                <p>The Roop Corridor serves as a modern take on the Thousand Pillars of the Madurai Temple, where Tamil art & literature flourished for generations. The six (6) enchanting rooms are designed in the same rhythmic flow bringing the joy of dance.</p>
+                <div class="wrap-btn">
+                    <button class="button enter-btn">ENTER
+                        <div class="all-btns"></div>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9 padding0">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <!--
+    <div class="item ">
+      <img src="<?php echo base_url('frontassets/image/slider/slide1.jpg'); ?>" alt="Chania">
+    </div>
+-->
+
+                    <div class="item active">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide2.jpg'); ?>" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide3.jpg'); ?>" alt="Flower">
+                    </div>
+
+                    <div class="item">
+                        <img src="<?php echo base_url('frontassets/image/slider/slide4.jpg'); ?>" alt="Flower">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class=" carousel-controls" href="#myCarousel" role="button" data-slide="prev">
+  <img class="left" src="<?php echo base_url('frontassets/image/left.jpg'); ?>" alt="Flower">
+
+  </a>
+                <a class=" carousel-controls" href="#myCarousel" role="button" data-slide="next">
+   <img class="right" src="<?php echo base_url('frontassets/image/right.jpg'); ?>" alt="Flower">
+<!--    <span class="sr-only">Next</span>-->
+  </a>
+            </div>
+        </div>
+    </div>
+    <div class="row mrg">
+        <div class="col-md-offset-3 col-md-6">
+            <div class="col-md-6 col-sm-6">
+                <div class="col-xs-6">
+                    <div class="cal">
+                        <p>check in</p>
+                        <p class="date">12</p>
+                        <p class="date-bold">SEPTEMBER ’15</p>
+                        <img src="<?php echo base_url('frontassets/image/amenites/border.png'); ?>" />
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xs-6">
+                    <div class="cal">
+                        <p>check in</p>
+                        <p class="date">17</p>
+                        <p class="date-bold">OCTOBER ’15</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="box">
+                    <a href="">CHECK AVAILABILITY</a>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
