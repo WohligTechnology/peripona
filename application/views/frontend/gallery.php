40aa037d237f53b9f5a9ec603645df27a4993180
Avinash <avinashghare572@gmail.com>
2015-07-15 12:02:18 +0530

    git upload removed

--- application/views/frontend/gallery.php
+++ application/views/frontend/gallery.php
@@ -12,19 +12,33 @@
         <p>
             We invite our leisure and business travelers to become immersed in the charm and intrigue of Design Hotel Chennai’s contemporary art styles infused with traditional South Indian Art. Welcome home! </p>
     </div>
+    <?php
+    print_r($images);
+    ?>
     <div class="row">
+       <?php
+        foreach($images as $image);
+        {
+            print_r($image);
+            $img=$image->image;
+        ?>
         <div class="col-md-4">
             <div class="img-gallery">
-                <a class="fancybox-thumb" rel="fancybox-thumb" href="<?php echo base_url('frontassets/image//gallery/gallery1.jpg'); ?>">
-          <img src="<?php echo base_url('frontassets/image//gallery/gallery1.jpg'); ?>" class="img-responsive"/></a>
+                <a class="fancybox-thumb" rel="fancybox-thumb" href="<?php echo base_url('uploads/'.$img); ?>">
+          <img src="<?php echo base_url('uploads/'.$img); ?>" class="img-responsive"/></a>
             </div>
         </div>
+        <?php
+        }
+        ?>
+<!--
         <div class="col-md-4">
             <div class="img-gallery">
                 <a class="fancybox-thumb" rel="fancybox-thumb" href="<?php echo base_url('frontassets/image//gallery/gallery2.jpg'); ?>">
           <img src="<?php echo base_url('frontassets/image//gallery/gallery2.jpg'); ?>" class="img-responsive"/></a>
             </div>
         </div>
+-->
     </div>
 
 </div>
